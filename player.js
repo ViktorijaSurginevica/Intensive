3af04976414c53eff4cd7fde4c3a2b17cb177f91
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-09-25 23:10:17 +0300

    track2

--- player.js
+++ player.js
@@ -48,7 +48,39 @@ const data = {
         coverImgUrl: "img/cardImage/image2.jpeg",
         trackCount: 4,
       },
-      tracks: [],
+      tracks: [
+        {
+          trackId: "1",
+          artistsName: "Eminem",
+          trackTitle: "Rap God",
+          isHot: true,
+          trackFileUrl: "audio/Eminem - Rap God.mp3",
+          trackCoverImgUrl: "img/cardImage/trackList/track1.jpeg",
+        },
+        {
+          trackId: "1",
+          artistsName: "50 cent",
+          trackTitle: "In da Club",
+          isHot: true,
+          trackFileUrl: "audio/50cent%20-%20In%20da%20club.mp3",
+          trackCoverImgUrl: "img/cardImage/trackList/track2.jpeg",
+        },
+        {
+          artistsName: "DMX",
+          trackTitle: "X Gon' Give It To Ya",
+          isHot: true,
+          trackFileUrl: "audio/DMX%20-%20X%20Gon'%20Give%20It%20To%20Ya.mp3",
+          trackCoverImgUrl: "img/cardImage/trackList/track3.jpeg",
+        },
+        {
+          artistsName: "Eminem feat 50 Cent, Cashis, Lloyd Banks",
+          trackTitle: "You Don't",
+          isHot: true,
+          trackFileUrl:
+            "audio/Eminem%20-%20You%20Don't%20Know%20(feat.%2050%20cent,%20Lloyd%20Banks%20and%20Cashis).mp3",
+          trackCoverImgUrl: "img/cardImage/trackList/track4.jpeg",
+        },
+      ],
     },
   ],
 };
