cecbf31a3217e9ed0909ed944dc91287ad846cf1
”ViktorijaSurginevica” <viafuture89@gmail.com>
2024-09-25 23:08:20 +0300

    first commit

---
+++ js/player.js
@@ -0,0 +1,93 @@
+const data = {
+    playlists: [
+        {
+            playlistInfo: {
+                title: "Hip-hop hits",
+                coverImgUrl: "img/cardImage/image1.jpeg",
+                tracksCount: 4
+            },
+            tracks: [
+                {
+                    trackId: "1",
+                    artistName: "Eminem",
+                    trackTitle: "Rap God",
+                    isHot: true,
+                    trackFileUrl: "audio/Eminem - Rap God.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track1.jpeg"
+                },
+                {
+                    trackId: "2",
+                    artistName: "50cent",
+                    trackTitle: "In da Club",
+                    isHot: false,
+                    trackFileUrl: "audio/50cent - In da club.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track2.jpeg"
+                },
+                {
+                    trackId: "3",
+                    artistName: "DMX",
+                    trackTitle: "X Gon' Give It To Ya",
+                    isHot: true,
+                    trackFileUrl: "audio/DMX - X Gon' Give It To Ya.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track2.jpeg"
+                },
+                {
+                    trackId: "4",
+                    artistName: "Eminem feat 50 Cent, Cashis, Lloyd Banks",
+                    trackTitle: "You Don't Know",
+                    isHot: true,
+                    trackFileUrl: "audio/Eminem - You Don't Know (feat. 50 cent, Lloyd Banks and Cashis).mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track3.jpeg"
+                },
+
+            ]
+        },
+        {
+            playlistInfo: {
+                title: "Rap Hits 1990s",
+                coverImgUrl: "img/cardImage/image2.jpeg",
+                tracksCount: 4
+            },
+            tracks: [
+                {
+                    trackId: "5",
+                    artistName: "Public Enemy",
+                    trackTitle: "Fight The Power",
+                    isHot: true,
+                    trackFileUrl: "audio/Public Enemy - Fight The Power.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track5.jpeg"
+                },
+                {
+                    trackId: "6",
+                    artistName: "Vanila Ice",
+                    trackTitle: "Ice Ice Baby",
+                    isHot: false,
+                    trackFileUrl: "audio/Vanila Ice - Ice Baby.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track6.jpeg"
+                },
+                {
+                    trackId: "7",
+                    artistName: "MC Hammer",
+                    trackTitle: "You Can’t Touch This",
+                    isHot: true,
+                    trackFileUrl: "audio/Mc Hammer - You Can't Touch This.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track7.jpeg"
+                },
+                {
+                    trackId: "8",
+                    artistName: "Brand Nubian",
+                    trackTitle: "Brand Nubian",
+                    isHot: true,
+                    trackFileUrl: "audio/Brand Nubian - Brand Nubian.mp3",
+                    trackCoverImgUrl: "img/cardImage/trackList/track8.jpeg"
+                },
+            ]
+        }
+    ]
+}
+
+
+
+
+
+
---
+++ player.js
@@ -0,0 +1,54 @@
+const data = {
+  playlists: [
+    {
+      playlistInfo: {
+        id: "1-0",
+        title: "Hip-Hop Hits",
+        coverImgUrl: "img/cardImage/image1.jpeg",
+        trackCount: 4,
+      },
+      tracks: [
+        {
+          trackId: "1",
+          artistsName: "Eminem",
+          trackTitle: "Rap God",
+          isHot: true,
+          trackFileUrl: "audio/Eminem - Rap God.mp3",
+          trackCoverImgUrl: "img/cardImage/trackList/track1.jpeg",
+        },
+        {
+          trackId: "1",
+          artistsName: "50 cent",
+          trackTitle: "In da Club",
+          isHot: true,
+          trackFileUrl: "audio/50cent%20-%20In%20da%20club.mp3",
+          trackCoverImgUrl: "img/cardImage/trackList/track2.jpeg",
+        },
+        {
+          artistsName: "DMX",
+          trackTitle: "X Gon' Give It To Ya",
+          isHot: true,
+          trackFileUrl: "audio/DMX%20-%20X%20Gon'%20Give%20It%20To%20Ya.mp3",
+          trackCoverImgUrl: "img/cardImage/trackList/track3.jpeg",
+        },
+        {
+          artistsName: "Eminem feat 50 Cent, Cashis, Lloyd Banks",
+          trackTitle: "You Don't",
+          isHot: true,
+          trackFileUrl:
+            "audio/Eminem%20-%20You%20Don't%20Know%20(feat.%2050%20cent,%20Lloyd%20Banks%20and%20Cashis).mp3",
+          trackCoverImgUrl: "img/cardImage/trackList/track4.jpeg",
+        },
+      ],
+    },
+    {
+      playlistInfo: {
+        id: "1-1",
+        title: "Rap Hits 1990s",
+        coverImgUrl: "img/cardImage/image2.jpeg",
+        trackCount: 4,
+      },
+      tracks: [],
+    },
+  ],
+};
